da0a82af7f9f108136dc72785732473d54039e43
Konrad Czarnota <k.czarnota27@gmail.com>
2017-07-05 19:22:48 +0200

    Features normalization

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -26,9 +26,10 @@ sigma = zeros(1, size(X, 2));
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
 
-
-
-
+mu = mean(X);
+sigma = std(X);
+X = X - repmat(mu, rows(X), 1);
+X = X ./ repmat(sigma, rows(X), 1);
 
 
 
