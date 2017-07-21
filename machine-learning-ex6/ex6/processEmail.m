57197de33e237c3300e020d8ffc28783d86c0324
Konrad Czarnota <k.czarnota27@gmail.com>
2017-07-21 20:27:27 +0200

    Implement email preprocessing

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,13 +97,12 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
-
+    for i=1:length(vocabList)
+        str_voc = vocabList{i};
+        if(strcmp(str, str_voc) == 1)
+            word_indices = [word_indices ; i];
+        end
+    end
 
 
     % =============================================================
