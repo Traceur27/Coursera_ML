6134aa945f2df2dc8231a1c57a498cd7317607ed
Konrad Czarnota <k.czarnota27@gmail.com>
2017-07-24 17:29:44 +0200

    Compute centroids

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,12 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
-
-
-
-
-
+for i=1:K
+    subset = X(idx == i, :);
+    centroids(i, :) = sum(subset)/size(subset, 1);
+end
 
 % =============================================================
 
